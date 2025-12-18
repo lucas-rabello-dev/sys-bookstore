db010b18258e9aa30326ae8767e617716809fe7c
lucas-rabello-dev <lucasrabello2020@gmail.com>
2025-12-18 13:11:56 -0300

    Completa issue 'servir paginas html'

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,12 @@ import (
 
 const (
 	HomeWay string = "./templates/home/home.html"
-	CrudPageWay string = "./templates/crudPage/crudPage.html"
+	CrudPageWay string = "./templates/cadastro-livro/cadastro-livro.html"
+	LoginWay string = "./templates/login/login.html"
+	MultasWay string = "./templates/multas/multas.html"
+	EstoqueWay string = "./templates/estoque/estoque.html"
+	EmprestimosWay string = "./templates/emprestimos/emprestimos.html"
+	BloqueadosWay string = "./templates/bloqueados/bloqueados.html"
 )
 
 var (
@@ -34,13 +39,29 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	// servindo a pasta static
 	mux = server.ServeStaticFiles(mux)
 
 	mux.HandleFunc("/home/", HandleHome)
 	success.Println("'home' carregada ✅")
 
-	mux.HandleFunc("/home/crudPage/", HandleCrud)
-	success.Println("'crudPage' carregado ✅")
+	mux.HandleFunc("/home/cadastro-livro/", HandleCadastroLivro)
+	success.Println("'cadastro-livro' carregado ✅")
+
+	mux.HandleFunc("/", HandleLogin)
+	success.Println("'login' carregado ✅")
+
+	mux.HandleFunc("/home/multas/", HandleMultas)
+	success.Println("'multas' carregado ✅")
+
+	mux.HandleFunc("/home/estoque/", HandleEstoque)
+	success.Println("'estoque' carregado ✅")
+
+	mux.HandleFunc("/home/emprestimos/", HandleEmprestimos)
+	success.Println("'emprestimos' carregado ✅")
+
+	mux.HandleFunc("/home/bloqueados/", HandleBloqueados)
+	success.Println("'bloqueados' carregado ✅")
 
 	fmt.Printf("Rodando em: http://%s:%s\n", host, port)
 	http.ListenAndServe(":" + port, mux)
@@ -66,31 +87,162 @@ func LoadDotEnv() (string, string, error) {
 }
 
 func HandleHome(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.Header().Set("Allow", http.MethodGet)
-		http.Error(w, "método inválido | método é diferente de GET", http.StatusMethodNotAllowed)
-	}
+	switch r.URL.Path {
+	case "/home", "/home/":
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodGet)
+			http.Error(w, "método inválido | método é diferente de GET", http.StatusMethodNotAllowed)
+			return
+		}
 
-	t, err := template.ParseFiles(HomeWay)
-	if err != nil {
-		http.Error(w, "erro ao carregar o html", http.StatusInternalServerError)
-	}
+		t, err := template.ParseFiles(HomeWay)
+		if err != nil {
+			http.Error(w, "erro ao carregar o html", http.StatusInternalServerError)
+			err_.Println("Erro ao carregar html em:", r.URL.Path)
+			return
+		}
 
-	w.WriteHeader(http.StatusOK)
-	t.ExecuteTemplate(w, "home", nil)
+		w.WriteHeader(http.StatusOK)
+		t.ExecuteTemplate(w, "home", nil)
+	default:
+		http.NotFound(w, r)
+	}
 }
 
-func HandleCrud(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.Header().Set("Allow", http.MethodGet)
-		http.Error(w, "método inválido | método é diferente de GET", http.StatusMethodNotAllowed)
-	}
+func HandleCadastroLivro(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/home/cadastro-livro", "/home/cadastro-livro/":
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodGet)
+			http.Error(w, "método inválido | método é diferente de GET", http.StatusMethodNotAllowed)
+			return
+		}
 
-	t, err := template.ParseFiles(CrudPageWay)
-	if err != nil {
-		http.Error(w, "erro ao carregar o html", http.StatusInternalServerError)
-	}
+		t, err := template.ParseFiles(CrudPageWay)
+		if err != nil {
+			http.Error(w, "erro ao carregar o html", http.StatusInternalServerError)
+			err_.Println("Erro ao carregar html em:", r.URL.Path)
+			return
+		}
 
-	w.WriteHeader(http.StatusOK)
-	t.ExecuteTemplate(w, "crudPage", nil)
+		w.WriteHeader(http.StatusOK)
+		t.ExecuteTemplate(w, "cadastro-livro", nil)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func HandleLogin(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodGet)
+			http.Error(w, "método inválido | método diferente de GET", http.StatusMethodNotAllowed)
+			return
+		}
+
+		t, err := template.ParseFiles(LoginWay)
+		if err != nil {
+			http.Error(w, "erro ao carregar html", http.StatusInternalServerError)
+			err_.Println("Erro ao carregar html em:", r.URL.Path)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		t.ExecuteTemplate(w, "login", nil)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func HandleMultas(w http.ResponseWriter, r *http.Request) {
+    switch r.URL.Path {
+    case "/home/multas", "/home/multas/":
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodGet)
+			http.Error(w, "método inválido | método diferente de GET", http.StatusMethodNotAllowed)
+			return
+		}
+
+		t, err := template.ParseFiles(MultasWay)
+		if err != nil {
+			http.Error(w, "erro ao carregar html", http.StatusInternalServerError)
+			err_.Println("Erro ao carregar html em:", r.URL.Path)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		t.ExecuteTemplate(w, "multas", nil)
+    default:
+        http.NotFound(w, r)
+    }
+}
+
+func HandleEstoque(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/home/estoque", "/home/estoque/":
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodGet)
+			http.Error(w, "método inválido | método diferente de GET", http.StatusMethodNotAllowed)
+			return
+		}
+
+		t, err := template.ParseFiles(EstoqueWay)
+		if err != nil {
+			http.Error(w, "erro ao carregar html", http.StatusInternalServerError)
+			err_.Println("Erro ao carregar html em:", r.URL.Path)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		t.ExecuteTemplate(w, "estoque", nil)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func HandleEmprestimos(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/home/emprestimos", "/home/emprestimos/":
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodGet)
+			http.Error(w, "método inválido | método diferente de GET", http.StatusMethodNotAllowed)
+			return
+		}
+
+		t, err := template.ParseFiles(EmprestimosWay)
+		if err != nil {
+			http.Error(w, "erro ao carregar html", http.StatusInternalServerError)
+			err_.Println("Erro ao carregar html em:", r.URL.Path)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		t.ExecuteTemplate(w, "emprestimos", nil)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func HandleBloqueados(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/home/bloqueados", "/home/bloqueados/":
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodGet)
+			http.Error(w, "método inválido | método diferente de GET", http.StatusMethodNotAllowed)
+			return
+		}
+
+		t, err := template.ParseFiles(BloqueadosWay)
+		if err != nil {
+			http.Error(w, "erro ao carregar html", http.StatusInternalServerError)
+			err_.Println("Erro ao carregar html em:", r.URL.Path)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		t.ExecuteTemplate(w, "bloqueados", nil)
+	default:
+		http.NotFound(w, r)
+	}
 }
