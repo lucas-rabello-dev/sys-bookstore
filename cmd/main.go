c1ae5471c60eeef014ad212efcd3ba8452ca6083
lucas-rabello-dev <lucasrabello2020@gmail.com>
2026-01-27 18:30:58 -0300

    Update html template

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
 var (
 	success *color.Color = color.New(color.FgGreen).Add(color.Bold, color.Italic)
 	err_ *color.Color = color.New(color.FgRed).Add(color.Bold, color.Italic)
@@ -22,36 +21,37 @@ func main() {
 	port, host, err := LoadDotEnv()
 	if err != nil {
 		// log.Fatal(err)
-		err_.Printf("Error: %v ❌\n", err)
+		err_.Printf("Error: %v [!]\n", err)
 		return
 	}
-	success.Println(".env carregado ✅")
+	success.Println(".env carregado [OK]")
 
 	mux := http.NewServeMux()
 
 	// servindo a pasta static
 	mux = server.ServeStaticFiles(mux)
+	
 
 	mux.HandleFunc("/home/", handlers.HandleHome)
-	success.Println("'home' carregada ✅")
+	success.Println("'home' carregada [OK]")
 
 	mux.HandleFunc("/home/cadastro-livro/", handlers.HandleCadastroLivro)
-	success.Println("'cadastro-livro' carregado ✅")
+	success.Println("'cadastro-livro' carregado [OK]")
 
 	mux.HandleFunc("/", handlers.HandleLogin)
-	success.Println("'login' carregado ✅")
+	success.Println("'login' carregado [OK]")
 
 	mux.HandleFunc("/home/multas/", handlers.HandleMultas)
-	success.Println("'multas' carregado ✅")
+	success.Println("'multas' carregado [OK]")
 
 	mux.HandleFunc("/home/estoque/", handlers.HandleEstoque)
-	success.Println("'estoque' carregado ✅")
+	success.Println("'estoque' carregado [OK]")
 
 	mux.HandleFunc("/home/emprestimos/", handlers.HandleEmprestimos)
-	success.Println("'emprestimos' carregado ✅")
+	success.Println("'emprestimos' carregado [OK]")
 
 	mux.HandleFunc("/home/bloqueados/", handlers.HandleBloqueados)
-	success.Println("'bloqueados' carregado ✅")
+	success.Println("'bloqueados' carregado [OK]")
 
 	fmt.Printf("Rodando em: http://%s:%s\n", host, port)
 	http.ListenAndServe(":" + port, mux)
